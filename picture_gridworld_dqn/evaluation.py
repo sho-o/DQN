31a42115dab35bab721e74a50344559ab3779ff7
sho-o <onst2025@gmail.com>
2017-04-02 18:26:25 +0900

    evaluation

---
+++ picture_gridworld_dqn/evaluation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import agent
+import environment
+import copy
+
+class Evaluation():
+	def __init__(self, comment, pic_kind, s_init, actions, max_step, exp_policy, net_type, gpu, pic_size, num_of_actions, memory_size, input_slides, batch_size, discount, rms_eps, rms_lr, optimizer_type):
+		self.env = environment.Environment(pic_kind)
+		self.agt = agent.Agent(exp_policy, net_type, gpu, pic_size, num_of_actions, memory_size, input_slides, batch_size, discount, rms_eps, rms_lr, optimizer_type)
+		self.agt.epsilon = 0
+		self.comment = comment
+		self.s_init = s_init
+		self.actions = actions
+		self.max_step = max_step
+		f = open("result/{}/evaluation/evaluation.csv".format(comment), "a")
+		f.write("episode,total_step,reward,episode_step\n")
+		f.close()
+
+	def __call__(self, q, learning_episode, learning_total_step):
+		self.agt.q = q
+		for episode in range(1):
+			self.env.make_episode_pics()
+			episode_reward = 0
+			s = self.s_init
+			pic_s = self.env.s_to_pic(s)
+
+			for steps in range(self.max_step):
+				a, _ = self.agt.policy(pic_s)
+				new_s = self.env.generate_next_s(s, self.actions[a])
+				pic_new_s = self.env.s_to_pic(new_s)
+				r = self.env.make_reward(s, self.actions[a])
+				done = self.env.judge_finish(new_s)
+				episode_reward += r
+				if done:
+					break
+				#prepare next s
+				s = new_s[:]
+				pic_s = np.array(pic_new_s)
+
+		self.make_eval_log(self.comment, learning_episode, learning_total_step, episode_reward, steps)
+
+	def make_eval_log(self, comment, learning_episode, learning_total_step, episode_reward, steps):
+		f = open("result/{}/evaluation/evaluation.csv".format(comment), "a")
+		f.write(str(learning_episode+1) + "," + str(learning_total_step) + "," + str(episode_reward) + "," + str(steps+1) + "\n")
+		f.close()
--- picture_gridworld_dqn/main.py
+++ picture_gridworld_dqn/main.py
@@ -6,6 +6,7 @@ import argparse
 import time
 import os
 import sys
+import evaluation
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--comment', '-c', default='', type=str, help='comment to distinguish output')
@@ -19,6 +20,7 @@ parser.add_argument('--finish_step', '-fs', default=5*10**7, type=int, help='end
 parser.add_argument('--q_update_freq', '-q', default=4, type=int, help='q update freaquency')
 parser.add_argument('--fixed_q_update_freq', '-f', default=10**4, type=int, help='fixed q update freaquency')
 parser.add_argument('--save_freq', '-sf', default=5*10**4, type=int, help='save frequency')
+parser.add_argument('--eval_freq', '-ef', default=10**4, type=int, help='evaluatuin frequency')
 parser.add_argument('--print_freq', '-pf', default=1, type=int, help='print result frequency')
 parser.add_argument('--initial_exploration', '-i', default=5*10**4, type=int, help='number of initial exploration')
 parser.add_argument('--batch_size', '-b', type=int, default=32, help='learning minibatch size')
@@ -45,6 +47,7 @@ def run(args):
 	q_update_freq = args.q_update_freq
 	fixed_q_update_freq = args.fixed_q_update_freq
 	save_freq = args.save_freq
+	eval_freq = args.eval_freq
 	print_freq = args.print_freq
 	initial_exploration = args.initial_exploration
 	batch_size = args.batch_size
@@ -61,13 +64,14 @@ def run(args):
 	epsilon_decrease_wide = 0.9/(epsilon_decrease_end - initial_exploration)
 
 	run_start = time.time()
-	make_directries(comment, ["network", "log"])
+	make_directries(comment, ["network", "log", "evaluation"])
 	if gpu >= 0:
 		cuda.get_device(gpu).use()
 	env = environment.Environment(pic_kind)
 	actions = ["up", "down", "right", "left"] 
 	num_of_actions = len(actions)
 	agt = agent.Agent(exp_policy, net_type, gpu, pic_size, num_of_actions, memory_size, input_slides, batch_size, discount, rms_eps, rms_lr, optimizer_type)
+	eva = evaluation.Evaluation(comment, pic_kind, s_init, actions, max_step, exp_policy, net_type, gpu, pic_size, num_of_actions, memory_size, input_slides, batch_size, discount, rms_eps, rms_lr, optimizer_type)
 	total_step = 0
 	
 	for episode in range(max_episode):
@@ -101,6 +105,9 @@ def run(args):
 				if total_step % save_freq == 0:
 					print "----------------------- save the_model ------------------------------"
 					serializers.save_npz('result/{}/network/q.net'.format(comment), agt.q)
+				if total_step % eval_freq == 0:
+					print "----------------------- evaluate the_model ------------------------------"
+					eva(agt.q, episode, total_step)
 				agt.epsilon = max(0.1, agt.epsilon - epsilon_decrease_wide)
 
 			#log, print_result
@@ -123,12 +130,14 @@ def make_directries(comment, dirs):
 			os.makedirs("result/{}/".format(comment) + d)
 
 def make_log(comment, episode, episode_reward, episode_average_value, epsilon, steps, total_step, run_time):
-	f = open("result/{}/log/log.txt".format(comment), "a")
-	f.write(str(episode+1) + "," + str(episode_reward) + "," + str(episode_average_value) + "," + str(epsilon) + ',' + str(steps) + ',' + str(total_step) + ',' + str(run_time) + "\n")
+	f = open("result/{}/log/log.csv".format(comment), "a")
+	if episode == 0:
+		f.write("episode,reward,average_value,epsilon,episode_step,total_step,run_time\n")
+	f.write(str(episode+1) + "," + str(episode_reward) + "," + str(episode_average_value) + "," + str(epsilon) + ',' + str(steps+1) + ',' + str(total_step) + ',' + str(run_time) + "\n")
 	f.close()
 
 def print_result(episode, steps, episode_reward, episode_time, epsilon, total_step, run_time):
-	print("-------------------Episode {} finished after {} steps-------------------".format(episode+1, steps))
+	print("-------------------Episode {} finished after {} steps-------------------".format(episode+1, steps+1))
 	print ("episode_reward : {}".format(episode_reward))
 	print ("episode_time: {}".format(episode_time))
 	print ("epsilon : {}".format(epsilon))
